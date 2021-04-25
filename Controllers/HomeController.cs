389d69d204190d10db2ebfc493fd26d8a2c03edc
akmal <akmal@DESKTOP-UHCN0UT>
2021-04-25 14:46:23 +0500

    AccountController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace HotelAlif.Controllers
             return View();
         }
         public IActionResult About()
-        {
+        { 
             return View();
         }
         public IActionResult Privacy()
